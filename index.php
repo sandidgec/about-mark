458752a35ab43653ab1f74e22af7241f69586d9e
Mark Stern <MarkStern@gmail.com>
2015-07-13 16:10:24 -0600

    A better start

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head><title>This page is about Mark Stern.</title></head>
 	<body>
 		<p>Mark was born in Trenton, NJ and grew up across the Delaware river in Pennsylvania.<br>
-			Mark went to Hampshire college, in Amherst, MA and majored in Photograpahy. Hampshire's motto is <color="#8b0000"><i> Non Satis Scire</i></color>, <b>To Know Is Not Enough</b>.</p>
+			Mark went to Hampshire college, in Amherst, MA and majored in Photograpahy. Hampshire's motto is <color="red"><i> Non Satis Scire</i></color>, <b>To Know Is Not Enough</b>.</p>
 		<p>This is what's in Mark's head<br>
 			<img src="images/head.jpg" alt="MRI"</p>
 		<p>Many years of being a hockey goalie has led to <b>multiple</b> concussions.</p>
