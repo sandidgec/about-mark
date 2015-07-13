19fd59ae1a3cef1d24df02938c9a10ec3a571e6c
Mark Stern <MarkStern@gmail.com>
2015-07-13 15:50:27 -0600

    THis is very basic but a good start!

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 		<p>Mark was born in Trenton, NJ and grew up across the Delaware river in Pennsylvania.<br>
 			Mark went to Hampshire college, in Amherst, MA and majored in Photograpahy.</p>
 		<p>This is what's in Mark's head<br>
-			<img src="images/head.jpg" alt="MRI"</p>
+			<img src="images/head.jpg" alt=" MRI"</p>
 	</body>
 </html>
