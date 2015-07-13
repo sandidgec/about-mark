ecf2581af2993045e186f0b07117dee9568f7c69
Mark Stern <MarkStern@gmail.com>
2015-07-13 15:44:48 -0600

    THis is very basic but a good start!

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+	<head><title>This page is about Mark Stern.</title></head>
+	<body>
+		<p>Mark was born in Trenton, NJ and grew up across the Delaware river in Pennsylvania.<br>
+			Mark went to Hampshire college, in Amherst, MA and majored in Photograpahy.</p>
+		<p>This is what's in Mark's head<br>
+			<img src="images/head.jpg" alt="MRI"</p>
+	</body>
+</html>
